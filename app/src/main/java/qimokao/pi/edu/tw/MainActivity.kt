c1615592dbb9179ace04328afb6eb8e17ae5f29c
黎黃壽上機考 <lehoangtho25122004@gmail.com>
2024-12-09 14:29:11 +0800

    v0_黎黃壽

---
+++ app/src/main/java/qimokao/pi/edu/tw/MainActivity.kt
@@ -0,0 +1,47 @@
+package qimokao.pi.edu.tw
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import qimokao.pi.edu.tw.ui.theme.HuangshouqimokaoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HuangshouqimokaoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HuangshouqimokaoTheme {
+        Greeting("Android")
+    }
+}
