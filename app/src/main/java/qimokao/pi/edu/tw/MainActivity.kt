e9c697fbb412dee61431d75de985b9160e5ddf22
黎黃壽上機考 <lehoangtho25122004@gmail.com>
2024-12-09 15:53:00 +0800

    v2_黎黃壽

--- app/src/main/java/qimokao/pi/edu/tw/MainActivity.kt
+++ app/src/main/java/qimokao/pi/edu/tw/MainActivity.kt
@@ -10,8 +10,26 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import qimokao.pi.edu.tw.ui.theme.HuangshouqimokaoTheme
+import android.content.pm.ActivityInfo
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +38,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             HuangshouqimokaoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+                    FirstScreen()
                 }
             }
         }
@@ -31,17 +47,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HuangshouqimokaoTheme {
-        Greeting("Android")
+fun FirstScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xff95fe95)),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "2024期末上機考 (資管2B黎黃壽)",
+                fontSize = 20.sp,
+                color = Color.Black
+            )
+            Image(
+                painter = painterResource(id = R.drawable.class_b),
+                contentDescription = "Class A Image",
+                modifier = Modifier.height(250.dp).width(700.dp)
+            )
+            Text(
+                text = "遊戲持續時間 0 秒",
+                fontSize = 16.sp,
+                color = Color.Black
+            )
+            Text(
+                text = "您的成績 0 分",
+                fontSize = 16.sp,
+                color = Color.Black
+            )
+            Button(
+                onClick = {},
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text(text = "結束App")
+            }
+        }
     }
 }
+
